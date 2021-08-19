48d3981d9b4807f0be4cae583a7b5b11426a5c10
tykm <tylerkim01@gmail.com>
2021-08-18 20:19:15 -0400

    Half functional binary search. Works on even cases but not odd

---
+++ binary_search/binary_search.py
@@ -0,0 +1,46 @@
+from generate_ordered_array import generate
+
+ordered_array = generate(8)
+print(ordered_array)
+
+def binary_search_iter(arr: list[int], target: int):
+    low = 0
+    high = len(arr)
+    mid = (low + high) // 2
+    while arr[mid] != target:
+        if arr[mid] < target:
+            low = mid + 1
+            mid = (low + high) // 2
+        elif arr[mid] > target:
+            high = mid - 1
+            mid = (low + high) // 2
+    return(arr[mid])
+    
+             
+print(binary_search_iter(ordered_array, 2))
+
+'''
+Cases:
+Array Even:
+    Target Lower:
+        Target Even:
+            arr = 8
+            target = 2
+        Target Odd:
+            arr = 8
+            target = 3
+
+
+Array Odd:
+
+
+
+
+
+
+
+
+
+
+
+'''
---
+++ binary_search/generate_ordered_array.py
@@ -0,0 +1,5 @@
+def generate(size=100000):
+    ordered_array = []
+    for i in range(1, size+1):
+        ordered_array.append(i)
+    return ordered_array
